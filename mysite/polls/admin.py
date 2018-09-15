d8b49df2a46bf6ad32ef21aadf840e69504532ee
zcx-star <zcx_star@163.com>
2018-09-15 21:20:52 +0800

    Finish base function of shader

--- mysite/polls/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import Question, Shader
-
-admin.site.register(Question)
-admin.site.register(Shader)
--- star/shader/views.py
+++ star/shader/views.py
@@ -6,21 +6,26 @@ from .models import Shader
 
 def index(request):		
     shader_list = Shader.objects.order_by('shader_name')
-    if request.method == "POST":   
-    	try:
-        	selected_shader = shader_list.filter(shader_opaque = request.POST['selectOpaque'])
-    	except (KeyError, Shader.DoesNotExist):  
-            selected_shader = shader_list    
-        
-        try:
-            selected_shader_1 = shader_list.filter(shader_uv = request.POST['selectUV'])
-        except (KeyError, Shader.DoesNotExist):  
-            selected_shader_1 = shader_list
-     
-    	return render(request, 'shader/index.html', {'shader_list':shader_list,'selected_shader':selected_shader})
 
-    else:
-    	return render(request, 'shader/index.html', {'shader_list':shader_list,'selected_shader':shader_list})
+    if request.method == "POST":
+        selected_opaque = request.POST['selectOpaque'] 
+        selected_uv = request.POST['selectUV']
+
+        if selected_opaque  == 'all':
+            q1 = Shader.objects.all()
+        else:
+            q1 = Shader.objects.filter(shader_opaque = selected_opaque)
+
+        if selected_uv == 'all':
+            q2 = Shader.objects.all()
+        else:
+            selected_uv = int(selected_uv)
+            q2 = Shader.objects.filter(shader_uv = selected_uv )
+        
+        selected_shader = q1.intersection(q2)
+        return render(request, 'shader/index.html', {'shader_list':shader_list,'selected_shader':selected_shader, 'selected_opaque':selected_opaque, 'selected_uv':selected_uv})
+
+    return render(request, 'shader/index.html', {'shader_list':shader_list,'selected_shader':shader_list})
 
 def detail(request,shader_name):
     shader = get_object_or_404(Shader, pk = shader_name)
