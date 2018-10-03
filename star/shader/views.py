fe81dbb6ec54567eb65c56d59b85d6b49e7bf470
zcx-star <zcx_star@163.com>
2018-10-03 16:41:34 +0800

    finish search all, need to fix search SurfaceType

--- star/shader/views.py
+++ star/shader/views.py
@@ -4,30 +4,100 @@ from django import forms
 from django.utils import timezone
 from .models import Shader
 
+def switchBooleam(selected):
+    temp = 'all'
+    if selected == 'Ture':
+        temp = '1'
+    elif selected == 'False':
+        temp = '0'
+    return temp
+
 def index(request): 
     shader_list = Shader.objects.order_by('shader_name')
 
-    '''
-    v=0
-    sql = "SELECT * FROM shader_shader WHERE shader_vertexColor=%s" %(v)
-    b=Shader.objects.raw(sql)
-    '''
-
-
-
     if request.method=="POST":
-        selected_vertexColor = request.POST['selectVertexColor'] 
-        selected_uvSet = request.POST['selectUVSet']
-        selected_surfaceType = request.POST['selectSurfaceType']
+        selected_vertexColor  = request.POST['selectVertexColor']   
+        s_0 = switchBooleam(selected_vertexColor)       
+        selected_uvSet        = request.POST['selectUVSet']
+        selected_surfaceType  = request.POST['selectSurfaceType']
+        selected_complexity   = request.POST['selectComplexity']
+        selected_textures     = request.POST['selectTextures']
+        selected_diffuseTex   = request.POST['selectDiffuseTex']
+        selected_normalTex    = request.POST['selectNormalTex']
+        selected_SMDTex       = request.POST['selectSMDTex']
+        selected_uvTint       = request.POST['selectUvTint']
+        s_8 = switchBooleam(selected_uvTint)
+        selected_maskTint     = request.POST['selectMaskTint']
+        s_9 = switchBooleam(selected_maskTint)
+        selected_globalDirt   = request.POST['selectGlobalDirt']
+        s_10 = switchBooleam(selected_globalDirt)
+        selected_emissive     = request.POST['selectEmissive']
+        s_11 = switchBooleam(selected_emissive)
+        selected_wetness      = request.POST['selectWetness']
+        s_12 = switchBooleam(selected_wetness)
+        selected_parallax     = request.POST['selectParallax']
+        s_13 = switchBooleam(selected_parallax)
+        selected_retroReflect = request.POST['selectRetroReflect']
+        s_14 = switchBooleam(selected_retroReflect)
 
-        sql="SELECT * FROM shader_shader WHERE shader_surfaceType=%s" %(selected_surfaceType)
+        selected = [s_0,selected_uvSet,selected_surfaceType,selected_complexity,selected_textures,selected_diffuseTex,selected_normalTex,selected_SMDTex,s_8,s_9,s_10,s_11,s_12,s_13,s_14]
+        allNum = selected.count('all')
+
+        sql = "SELECT * FROM shader_shader"
+        attach = ""
+        judgeNum = 15
+        judgements = [
+        'shader_vertexColor',
+        'shader_uvSet',
+        'shader_surfaceType',
+        'shader_complexity',
+        'shader_textures',
+        'shader_diffuseTex',
+        'shader_normalTex',
+        'shader_SMDTex',
+        'shader_uvTint',
+        'shader_maskTint',
+        'shader_globalDirt',
+        'shader_emissive',
+        'shader_wetness',
+        'shader_parallax',
+        'shader_retroReflect']
+
+        if allNum == (judgeNum-1):
+            for i in range(judgeNum):
+                if selected[i] != 'all':
+                    attach += " WHERE "+judgements[i]+" = "+selected[i]
+        elif allNum != judgeNum:
+            attach += " WHERE "
+            for i in range(judgeNum):
+                if selected[i] != 'all':
+                    attach += judgements[i]+" = "+selected[i]+" AND "
+            attach = attach[:-5]
+
+        sql = sql + attach
+
         selected_shader=Shader.objects.raw(sql)
 
-        return render(request, 'shader/index.html', 
-        {'shader_list':shader_list,'selected_shader':selected_shader,
-        'selected_vertexColor':selected_vertexColor,'selected_uvSet':selected_uvSet,
-        'selected_surfaceType':selected_surfaceType
-        })
+        return render(request, 'shader/index.html',{
+            "shader_list":shader_list,
+            "selected_shader":selected_shader,
+            "selected_vertexColor":selected_vertexColor,
+            "selected_uvSet":selected_uvSet,
+            "selected_surfaceType":selected_surfaceType,
+            "selected_complexity":selected_complexity,
+            "selected_textures":selected_textures,
+            "selected_diffuseTex":selected_diffuseTex,
+            "selected_normalTex":selected_normalTex,
+            "selected_SMDTex":selected_SMDTex,
+            "selected_uvTint":selected_uvTint,
+            "selected_maskTint":selected_maskTint,
+            "selected_globalDirt":selected_globalDirt,
+            "selected_emissive":selected_emissive,
+            "selected_wetness":selected_wetness,
+            "selected_parallax":selected_parallax,
+            "selected_retroReflect":selected_retroReflect,
+            'sql':sql,
+            'allNum':allNum})
 
     return render(request, 'shader/index.html', {'shader_list':shader_list,'selected_shader':shader_list})
 
